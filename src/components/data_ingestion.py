ed392ea9fbb490574e635902aecee4cb8c5f7cc3
priyanshudeora <deorapriyanshu@gmail.com>
2025-02-14 22:27:39 +0530

    hypermetertuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformtion import DataTransformation
 from src.components.data_transformtion import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
     test_data_path: str=os.path.join('artifacts','test.csv')
@@ -50,6 +53,9 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-    
-    data_transfomation=DataTransformation()
-    data_transfomation.initiate_data_transformation(train_data,test_data)
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
